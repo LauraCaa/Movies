74308c90e5dc8f06d02e30caac7cc16e81e8336b
Camila <cardenascastrolauracamila7@gmail.com>
2025-08-05 20:47:51 -0500

    feat: set up React app entry point with routing and Redux store

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,38 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './index.css';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from "./componets/ErrorPage";
+import Root from "./routes/Root";
+import Login from "./routes/Login";
+import Movies from "./routes/Movies";
+import MoviesDetail, {loader as detailLoader} from "./routes/MovieDetail";
+import store from './app/store';
+
+const router = createBrowserRouter([
+  {
+    element: <Root/>,
+    errorElement:<ErrorPage/>,
+    children: [
+      {
+        path: "/",
+        element: <Movies/>
+      },
+      {
+        path: "login",
+        element: <Login/>
+      },
+      {
+        path: "/:id",
+        element: <MoviesDetail/>,
+        loader: detailLoader, 
+      }
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
-  <App />
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 )
