419846cd0fe6daf9da77737b731e9192929a94bd
Camila <cardenascastrolauracamila7@gmail.com>
2025-08-05 20:53:21 -0500

    feat: add auth and movies slices with async movie fetching

---
+++ src/features/moviesSlice.jsx
@@ -0,0 +1,36 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import {getMovies} from "../services/apiCall";
+
+const initialState= {
+    movieList: [],
+    loading: false,
+    error: false,
+};
+
+export const fetchMovies = createAsyncThunk("movies/fetchMovies", async()=>{
+    return await getMovies()
+});
+
+const moviesSlice = createSlice({
+    name:"movies",
+    initialState,
+    reducers:{
+        clearMoviesList: (state) => {
+            state.movieList= []
+        }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(fetchMovies.pending, (state) => {
+            state.loading = true;
+        }).addCase(fetchMovies.fulfilled, (state, {payload}) => {
+            state.loading = false;
+            state.movieList = payload;
+        }).addCase(fetchMovies.rejected, (state) => {
+            state.loading = false;
+            state.error = action.error.message || "Error desconocido";
+        })
+    }
+});
+
+export const { clearMoviesList } = moviesSlice.actions;
+export default moviesSlice.reducer;
